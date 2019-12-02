5706bf2b925a3090b55907c49fb9be0be9510097
shahid37 <shahidhumeed.com>
2019-12-02 21:33:48 +0500

    state change of a string on pressing button

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui' as prefix0;
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -21,13 +23,45 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String  myText = "helo world";
+  void _changeText(){
+    setState(() {
+      if(myText.startsWith("h")) {
+        myText = "Welcome to helo world";
+      }
+      else{
+        myText = "helo world";
+      }
+      });
+  }
+
+  Widget _bodyWidget(){
+
+    return new Container(
+      padding: const EdgeInsets.all(10.0),
+      child: new Center(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[ 
+            new Text(myText,style: new TextStyle(color: Colors.pink,fontSize: 25),),
+            new RaisedButton(onPressed: _changeText,
+            color: Colors.pink,
+            textColor: Colors.white,
+            child: new Text("Click me",style: new TextStyle(fontWeight:FontWeight.bold,fontSize: 20.0),))
+          ],
+        ),
+      ),
+    );
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: new AppBar(
         title: new Text("Home Page"),
       ),
-      body: new Center(child: new Text("Helo Statefull Widget"),),
+      body: _bodyWidget(),
     );
   }
 }
