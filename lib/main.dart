ea773955f420d586634303142103ae8d1b2ca91b
shahid37 <shahidhumeed.com>
2019-12-03 16:05:51 +0500

    add image add container and column property and add raised button

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:ui' as prefix0;
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,64 +7,28 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-
-      title: "My App",
-      home: new HomePage(),
-      theme: new ThemeData(
-        primarySwatch: Colors.green,
-        accentColor: Colors.red
-      ),
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: new Text("My First App"),
+          ),
+          body: Column(children: [
+            Container(
+              margin: EdgeInsets.only(top: 50.0,bottom: 10.0),
+              child: RaisedButton(
+                child: Text("Clic me For Add Product"),
+                onPressed: () {},
+              ),
+            ),
+            Card(
+              child: Column(
+                children: <Widget>[
+                  Image.asset('assets/food.jpg'),
+                  Text("Food Paradise")
+                ],
+              ),
+            )
+          ])),
     );
   }
 }
-
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  String  myText = "helo world";
-  void _changeText(){
-    setState(() {
-      if(myText.startsWith("h")) {
-        myText = "Welcome to helo world";
-      }
-      else{
-        myText = "helo world";
-      }
-      });
-  }
-
-  Widget _bodyWidget(){
-
-    return new Container(
-      padding: const EdgeInsets.all(10.0),
-      child: new Center(
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[ 
-            new Text(myText,style: new TextStyle(color: Colors.pink,fontSize: 25),),
-          ],
-        ),
-      ),
-    );
-  }
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: new AppBar(
-        title: new Text("Home Page"),
-      ),
-      body: _bodyWidget(),
-      floatingActionButton: new FloatingActionButton(
-        child: new Icon(Icons.add),
-        onPressed: _changeText,
-      ),
-    );
-  }
-}
-
